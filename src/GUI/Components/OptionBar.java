2ebb0218d09e7cf999e4675cb7b5ef3f70ab7987
Caden Sumner <cadenjsumner@gmail.com>
2016-12-13 22:48:55 -0500

    Reformat cod

--- src/GUI/Components/OptionBar.java
+++ src/GUI/Components/OptionBar.java
@@ -1,20 +1,14 @@
 package GUI.Components;
 
-import GUI.Controller;
 import GUI.Styler;
 import Logger.Level;
 import Logger.Logger;
 import Maus.Maus;
 import Server.ServerSettings;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 
 import java.util.Random;
@@ -27,7 +21,7 @@ class OptionBar {
         menuBar.getStyleClass().add("background");
 
         Label maus = (Label) Styler.styleAdd(new Label("Maus 0.5a"), "option-button");
-        maus.setPadding(new Insets(5,10,5,10));
+        maus.setPadding(new Insets(5, 10, 5, 10));
         maus.setOnMouseClicked(event -> {
             String[] MausEsterEgg = {
                     "Maus 0.5a",
@@ -47,21 +41,26 @@ class OptionBar {
                     "Maus only continues if I get community feedback!",
                     "INF3CTED!!11oneone!1oen",
                     "Deditated Wam",
-                    "Meow"
+                    "Meow",
+                    "Luminosity Maus 1.5",
+                    "Spreche sie Deutsche?",
+                    "Telegram @Luxington",
+                    "Carrier pigeons are faster",
+                    "Duct Tape is more stable than this shit"
             };
             Random rn = new Random();
-            int rnn = rn.nextInt(MausEsterEgg.length + 1);
+            int rnn = rn.nextInt(MausEsterEgg.length);
             maus.setText(MausEsterEgg[rnn]);
         });
 
         Label minimize = (Label) Styler.styleAdd(new Label("_"), "option-button");
-        minimize.setPadding(new Insets(5,10,5,10));
+        minimize.setPadding(new Insets(5, 10, 5, 10));
         minimize.setOnMouseClicked(event -> {
             Maus.getPrimaryStage().setIconified(true);
         });
 
         Label exit = (Label) Styler.styleAdd(new Label("X"), "option-button");
-        exit.setPadding(new Insets(5,10,5,10));
+        exit.setPadding(new Insets(5, 10, 5, 10));
         exit.setOnMouseClicked(event -> {
             if (ServerSettings.isBackgroundPersistent()) {
                 Platform.exit();
@@ -83,10 +82,13 @@ class OptionBar {
             Maus.getPrimaryStage().setY(mouseEvent.getScreenY() + dragDelta.y);
         });
 
-        HBox hBox = Styler.hContainer(5, maus,sep, minimize,exit);
+        HBox hBox = Styler.hContainer(5, maus, sep, minimize, exit);
         hBox.setId("drag-bar");
         return hBox;
     }
-    class Delta { double x, y; }
+
+    class Delta {
+        double x, y;
+    }
 
 }
